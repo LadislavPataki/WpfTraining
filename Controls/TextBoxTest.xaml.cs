4297e36c5d143ba33a25267800b0e0de4711d877
Pataki, Ladislav <ladislav.pataki@erni.sk>
2017-02-20 13:31:57 +0100

    more controls

---
+++ Controls/TextBoxTest.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Controls
+{
+    /// <summary>
+    /// Interaction logic for TextBoxTest.xaml
+    /// </summary>
+    public partial class TextBoxTest : Window
+    {
+        public TextBoxTest()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (txtSelection == null) return;
+            txtSelection.Text = $"Selection from {txt.SelectionStart} to {txt.SelectionLength} is \"{txt.SelectedText}\"";
+        }
+    }
+}
